c6bd252e670bd6bef0214110804eff770e2c361e
rebeccasmall <rebecca.small18@hotmail.com>
2025-06-16 09:48:57 -0400

    Fetched from API & parsed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+    const userRequest = await fetch("https://randomuser.me/api?results=5");
+    const data = await userRequest.json();
+};
