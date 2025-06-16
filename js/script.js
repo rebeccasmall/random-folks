0305e987ab68954abdf6720d2c5ab8a8513200f5
rebeccasmall <rebecca.small18@hotmail.com>
2025-06-16 10:10:50 -0400

    Finished function for fetching user info

--- js/script.js
+++ js/script.js
@@ -3,4 +3,26 @@ const randomFolks = document.querySelector(".random-peeps");
 const getData = async function () {
     const userRequest = await fetch("https://randomuser.me/api?results=5");
     const data = await userRequest.json();
+    // console.log(data);
+    const userResults = data.results;
+    console.log(userResults);
+    displayUsers(userResults);
+};
+
+getData();
+
+const displayUsers = function (userResults) {
+    randomFolks.innerHTML = "";
+    userResults.forEach(function(user) {
+        const country = user.location.country;
+        const name = user.name.first;
+        const imageUrl = user.picture.medium;
+        // console.log(country, name, imageUrl);
+        let userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+        <h3>${name}</h3>
+        <p>${country}</p>
+        <img src=${imageUrl} alt="User avatar" />`;
+        randomFolks.append(userDiv);
+      });
 };
